da157eae65907529b36142b4c5e2e5a07e329b64
Karla <karla.ruan@cceo.com.mx>
2018-05-16 14:53:55 -0500

    Recortar imagenes

--- canvas.js
+++ canvas.js
@@ -7,6 +7,9 @@ image.src="./imagen.jpg";
 
 image.onload=function(){
 	console.log("La imagen fue cargada");
-	//ctx.draeImage(this o imagen, coordX para dibujar, coordY para dibujar, Tamaño de ancho, tamaño de largo)
-	ctx.drawImage(this,10,10,400,500);
+	//ctx.drawImagen(imagen,coordX,coordY)
+	//ctx.drawImage(imagen, coordX para dibujar, coordY para dibujar, ancho, alto)
+	//ctx.drawImagen(image,imageX,imageY,imagenAncho,imagenAlto,coordX,coordY,ancho,alto)
+	//ctx.drawImage(this,10,10,400,500);
+	ctx.drawImage(this,560,20,150,220,20,20,200,300);
 }
